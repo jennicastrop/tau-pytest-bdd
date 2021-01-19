832af48f518235b3f9ca094dffe300f063f75bce
Jennifer Castro Porras <jennicastro@gmail.com>
2021-01-19 17:01:21 -0600

    User partial to reduce step definitions code

--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -1,7 +1,8 @@
+from _functools import partial
 from pytest_bdd import scenarios, parsers, given, when, then
 
 from cucumbers import CucumberBasket
-
+import pytest
 
 scenarios('../features/cucumbers.feature')
 
@@ -10,22 +11,24 @@ EXTRA_TYPES = {
     'Number': int,
 }
 
+parse_num = partial(parsers.cfparse, extra_types=EXTRA_TYPES)
 
-@given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=EXTRA_TYPES))
+@pytest.fixture
+@given(parse_num('the basket has "{initial:Number}" cucumbers'))
 def basket(initial):
     return CucumberBasket(initial_count=initial)
 
 
-@when(parsers.cfparse('"{some:Number}" cucumbers are added to the basket', extra_types=EXTRA_TYPES))
+@when(parse_num('"{some:Number}" cucumbers are added to the basket'))
 def add_cucumbers(basket, some):
     basket.add(some)
 
 
-@when(parsers.cfparse('"{some:Number}" cucumbers are removed from the basket', extra_types=EXTRA_TYPES))
+@when(parse_num('"{some:Number}" cucumbers are removed from the basket'))
 def remove_cucumbers(basket, some):
     basket.remove(some)
 
 
-@then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=EXTRA_TYPES))
+@then(parse_num('the basket contains "{total:Number}" cucumbers'))
 def basket_has_total(basket, total):
     assert basket.count == total
